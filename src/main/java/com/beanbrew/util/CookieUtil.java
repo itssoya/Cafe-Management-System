f2336dd058223067f46479373fa56fcac5956cce
itssoya <itssourav.y@gmail.com>
2026-05-03 03:13:31 +0545

    Adding session and cookies

---
+++ src/main/java/com/beanbrew/util/CookieUtil.java
@@ -0,0 +1,41 @@
+package com.beanbrew.util;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieUtil {
+	
+	public static void addCookie(HttpServletResponse response, String name, String value, int ageMax ) {
+		
+		Cookie cookie = new Cookie(name, value);
+		cookie.setPath("/");
+		cookie.setMaxAge(ageMax);
+		cookie.setHttpOnly(true);
+		
+	}
+	
+	public static void deleteCookie(HttpServletResponse response, String name) {
+		
+		addCookie(response, name, "", 0);
+	}
+	
+	public static Optional<String> getCookieValue(HttpServletRequest request, String name){
+		
+		Cookie[] cookie = request.getCookies();
+		
+		if (cookie == null) {
+	        return Optional.empty();
+	    }
+		
+		return  Arrays.stream(cookie)
+				.filter(c -> name.equals(c.getName()))
+				.map(c-> c.getValue())
+				.findFirst();
+							
+	}
+
+}
---
+++ src/main/java/com/beanbrew/util/SessionUtil.java
@@ -0,0 +1,46 @@
+package com.beanbrew.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+public class SessionUtil {
+	
+	public static <T> void setAttribute(HttpServletRequest request, String name, T value, int seconds) {
+		
+		HttpSession session = request.getSession(true);
+		session.setAttribute(name, value);
+		session.setMaxInactiveInterval(seconds);
+	}
+	
+	public static <T> T setAttribute(HttpServletRequest request, String name, Class<T> type){
+		
+		HttpSession session = request.getSession(false);
+		if(session != null) {
+			Object obj =  session.getAttribute(name);
+			if(type.isInstance(obj)) {
+				return type.cast(obj);
+			}
+			
+		}
+		return null;
+	}
+	
+	public static void removeAttribute(HttpServletRequest request, String name) {
+		
+		HttpSession session = request.getSession(false);
+		
+		if(session != null) {
+			session.removeAttribute(name);
+		}
+	}
+	
+	public static void invalidateSession(HttpServletRequest request) {
+		
+		HttpSession session = request.getSession(false);
+		
+		if(session != null) {
+			session.invalidate();
+		}
+	}
+
+}
