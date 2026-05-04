5641958d57cb03e7ae1a5c665012bd6205bfc75e
itssoya <itssourav.y@gmail.com>
2026-05-04 13:07:02 +0545

    Signup without error handling

--- src/main/java/com/beanbrew/service/SignupService.java
+++ src/main/java/com/beanbrew/service/SignupService.java
@@ -1,16 +1,40 @@
 package com.beanbrew.service;
 
+import java.io.File;
+
 import com.beanbrew.dao.SignupDAO;
 import com.beanbrew.model.User;
 import com.beanbrew.util.PasswordUtil;
+import com.beanbrew.util.FileUploadUtil;
+
+
+import jakarta.servlet.http.Part;
 
 public class SignupService {
 	
-	public void addUser(User user) throws Exception {
+
+	private static final String UPLOAD_DIR = System.getProperty("user.home") + File.separator + "webapp_uploads";
+	
+	
+	public void addUser(User user, Part imagePart) throws Exception {
 		
 		String hashedPassword = PasswordUtil.hashPassword(user.getPassword());
 		
 		user.setPassword(hashedPassword);
+		
+		if(imagePart != null&& imagePart.getSize() > 0) {
+			
+			if(FileUploadUtil.isImage(imagePart) && FileUploadUtil.isValidType(imagePart)) {
+			
+				String extension = FileUploadUtil.getFileExtension(imagePart.getSubmittedFileName());
+				String fileName = "Img_" + System.currentTimeMillis() + extension;
+                FileUploadUtil.saveFile(imagePart, UPLOAD_DIR, fileName);
+                
+                String imageUrl =  fileName;
+        		user.setProfileImageURL(imageUrl);
+			
+			}
+		}
 		
 		SignupDAO dao = new SignupDAO();
 		dao.signUp(user);
